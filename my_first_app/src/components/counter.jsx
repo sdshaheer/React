7b935a91efdf7278cff795ffc12c9c51880f146a
syed shaheer <54505156+sdshaheer@users.noreply.github.com>
2022-08-18 15:00:20 +0530

    react app
    react app

---
+++ my_first_app/src/components/counter.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+class Counter extends Component {
+    
+    render() { 
+        return (
+        <div>
+            <span className={this.getBadgeClasses()}>{this.formatcount()}</span>
+            <button className="btn btn-secondary btn-sm" onClick={()=>this.props.onIncrement(this.props.counter)}>Increment</button>
+            <button className="btn btn-danger btn-sm m-2" onClick={()=>this.props.onDelete(this.props.counter.id)}>Delete</button>
+        </div>);
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.props.counter.value === 0 ? "warning" : "primary";
+        return classes;
+    }
+
+    formatcount(){
+        //const {count}=this.props.counter;
+        return this.props.counter.value===0?"Zero":this.props.counter.value;
+    }
+}
+export default Counter; 
